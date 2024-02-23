33533c62dd78a0595790135dfe63558a6a0239c0
AmelianHearts <amelianhearts@gmail.com>
2024-02-22 20:19:43 -0800

    8pm check-in

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    private BoxCollider2D boxCollider;
     private Rigidbody2D body;
+    private float horizontalInput;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
 
-        if(Input.GetKey(KeyCode.W))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        //Flip player when moving left-right
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3((float)0.7, (float)0.7, 1);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3((float) -0.7, (float) 0.7, 1);
+
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.W)&& isGrounded())
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
     }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+
 }
